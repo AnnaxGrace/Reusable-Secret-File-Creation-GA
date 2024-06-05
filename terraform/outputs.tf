e187c334f0861c478b569485b735bd0e17d0141a
annaxgrace <anna.grace.conover@gmail.com>
2024-06-05 12:26:32 -0500

    remved second

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "test_output_dynamic" {
     value = azurerm_resource_group.use_secret.name
 }
 
-output "test_output_dynamic2" {
-    value = azurerm_resource_group.use_secret.id
-}
+# output "test_output_dynamic2" {
+#     value = azurerm_resource_group.use_secret.id
+# }
